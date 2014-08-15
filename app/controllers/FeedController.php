cd63f00de870286626c2421b8e45710421133b03
Artem Zinoviev <arzinoviev@gmail.com>
2014-08-15 16:56:29 +0300

    add more feeds

--- app/controllers/FeedController.php
+++ app/controllers/FeedController.php
@@ -5,9 +5,10 @@ use Agregator\Helpers\XmlToJson;
 class FeedController extends \BaseController {
 
     const UNIAN = 'http://rss.unian.net/site/news_rus.rss';
-    const ICORPUS = 'http://icorpus.ru/feed/';
+    const LIFENEWS = 'http://lifenews.ru/xml/feed.xml';
     const RUSVESNA = 'http://rusvesna.su/rss.xml';
     const CENSOR = 'http://censor.net.ua/includes/resonance_full_ru.xml';
+    const LENTARU = 'http://lenta.ru/rss';
 
 	/**
 	 * Display a listing of the resource.
@@ -19,12 +20,13 @@ class FeedController extends \BaseController {
 	{
         $data = new XmlToJson;
 
-        list($json_unian_data, $json_icorpus_data, $json_rusvesna_data, $json_censor_data) = $this->parser($data);
+        list($json_lenta_data, $json_unian_data, $json_lifenews_data, $json_rusvesna_data, $json_censor_data) = $this->parser($data);
 
         return $this->createArrayFromJsonObject($json_unian_data,
-                                                $json_icorpus_data,
+                                                $json_lifenews_data,
                                                 $json_rusvesna_data,
-                                                $json_censor_data);
+                                                $json_censor_data,
+                                                $json_lenta_data);
 	}
 
     public function show()
@@ -39,24 +41,27 @@ class FeedController extends \BaseController {
     public function parser($data)
     {
         $json_unian_data = $data->parseXML(self::UNIAN);
-        $json_icorpus_data = $data->parseXML(self::ICORPUS);
+        $json_lenta_data = $data->parseXML(self::LENTARU);
+        $json_lifenews_data = $data->parseXML(self::LIFENEWS);
         $json_rusvesna_data = $data->parseXML(self::RUSVESNA);
         $json_censor_data = $data->parseXML(self::CENSOR);
-        return array($json_unian_data, $json_icorpus_data, $json_rusvesna_data, $json_censor_data);
+        return array($json_unian_data, $json_lenta_data, $json_lifenews_data, $json_rusvesna_data, $json_censor_data);
     }
 
     /**
      * @param $unian
-     * @param $icorpus
+     * @param $lifenews
+     * @param $lenta
      * @param $rusvesna
      * @param $censor
      * @return array
      */
-    public function createArrayFromJsonObject($unian, $icorpus, $rusvesna, $censor)
+    public function createArrayFromJsonObject($unian, $lifenews, $lenta, $rusvesna, $censor)
     {
         /* this magic with encode/decode is necessary for receiving correct merge json object */
         return array_merge_recursive(json_decode(json_encode($unian, JSON_UNESCAPED_UNICODE), true),
-                                    json_decode(json_encode($icorpus, JSON_UNESCAPED_UNICODE), true),
+                                    json_decode(json_encode($lifenews, JSON_UNESCAPED_UNICODE), true),
+                                    json_decode(json_encode($lenta, JSON_UNESCAPED_UNICODE), true),
                                     json_decode(json_encode($rusvesna, JSON_UNESCAPED_UNICODE), true),
                                     json_decode(json_encode($censor, JSON_UNESCAPED_UNICODE), true));
     }
