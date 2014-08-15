5ee21727733bf2c07d081e28ab8cbc57172be6c0
Artem Zinoviev <arzinoviev@gmail.com>
2014-08-15 17:00:01 +0300

    add adsense

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -13,6 +13,15 @@ scale=1.0">
     <!-- Bootstrap CSS -->
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="/css/main.css" rel="stylesheet" media="screen">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- test -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:300px;height:600px"
+         data-ad-client="ca-pub-8991474059210216"
+         data-ad-slot="9206148683"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </head>
 
 <body>
