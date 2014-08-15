733ee38cf0cdd6aef597485857060aa3b4b50934
Artem Zinoviev <arzinoviev@gmail.com>
2014-08-15 19:57:24 +0300

    add informer

--- app/controllers/FeedController.php
+++ app/controllers/FeedController.php
@@ -5,6 +5,7 @@ use Agregator\Helpers\XmlToJson;
 class FeedController extends \BaseController {
 
     const UNIAN = 'http://rss.unian.net/site/news_rus.rss';
+    const LIGA = 'http://news.liga.net/all/rss.xml';
     const LIFENEWS = 'http://lifenews.ru/xml/feed.xml';
     const RUSVESNA = 'http://rusvesna.su/rss.xml';
     const CENSOR = 'http://censor.net.ua/includes/resonance_full_ru.xml';
@@ -21,9 +22,15 @@ class FeedController extends \BaseController {
 	{
         $data = new XmlToJson;
 
-        list($json_lenta_data, $json_unian_data, $json_lifenews_data, $json_rusvesna_data, $json_censor_data) = $this->parser($data);
+        list($json_lenta_data,
+            $json_unian_data,
+            $json_lifenews_data,
+            $json_rusvesna_data,
+            $json_censor_data,
+            $json_liga_data) = $this->parser($data);
 
         return $this->createArrayFromJsonObject($json_unian_data,
+                                                $json_liga_data,
                                                 $json_lifenews_data,
                                                 $json_rusvesna_data,
                                                 $json_censor_data,
@@ -51,26 +58,29 @@ class FeedController extends \BaseController {
     public function parser($data)
     {
         $json_unian_data = $data->parseXML(self::UNIAN);
+        $json_liga_data = $data->parseXML(self::LIGA);
         $json_lenta_data = $data->parseXML(self::LENTARU);
         $json_lifenews_data = $data->parseXML(self::LIFENEWS);
         $json_rusvesna_data = $data->parseXML(self::RUSVESNA);
         $json_censor_data = $data->parseXML(self::CENSOR);
-        return array($json_unian_data, $json_lenta_data, $json_lifenews_data, $json_rusvesna_data, $json_censor_data);
+        return array($json_unian_data, $json_liga_data, $json_lenta_data, $json_lifenews_data, $json_rusvesna_data, $json_censor_data);
     }
 
     /**
      * @param $unian
      * @param $lifenews
+     * @param $novorossia
      * @param $lenta
      * @param $rusvesna
      * @param $censor
      * @return array
      */
-    public function createArrayFromJsonObject($unian, $lifenews, $lenta, $rusvesna, $censor)
+    public function createArrayFromJsonObject($unian, $lifenews, $novorossia, $lenta, $rusvesna, $censor)
     {
         /* this magic with encode/decode is necessary for receiving correct merge json object */
         return array_merge_recursive(json_decode(json_encode($unian, JSON_UNESCAPED_UNICODE), true),
                                     json_decode(json_encode($lifenews, JSON_UNESCAPED_UNICODE), true),
+                                    json_decode(json_encode($novorossia, JSON_UNESCAPED_UNICODE), true),
                                     json_decode(json_encode($lenta, JSON_UNESCAPED_UNICODE), true),
                                     json_decode(json_encode($rusvesna, JSON_UNESCAPED_UNICODE), true),
                                     json_decode(json_encode($censor, JSON_UNESCAPED_UNICODE), true));
--- app/views/pages/feed.blade.php
+++ app/views/pages/feed.blade.php
@@ -16,7 +16,41 @@ Page | feed
         <img class="img-loading" src="/img/ajax-loader.gif" id="img" alt=""/>
     </div>
 
-    <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9" ng-hide="loading">
+    <div class="col-sm-3 col-md-3 col-lg-3 hidden-xs" ng-hide="loading">
+
+        <!-- Gismeteo informer START -->
+        <link rel="stylesheet" type="text/css" href="http://www.gismeteo.ru/static/css/informer2/gs_informerClient.min.css">
+        <div id="gsInformerID-P2K37C0Nb81K4L" class="gsInformer" style="width:240px;height:182px">
+          <div class="gsIContent">
+           <div id="cityLink">
+             <a href="http://www.gismeteo.ru/city/daily/11855/" target="_blank">Погода в Макеевке</a>
+           </div>
+           <div class="gsLinks">
+             <table>
+               <tr>
+                 <td>
+                   <div class="leftCol">
+                     <a href="http://www.gismeteo.ru" target="_blank">
+                       <img alt="Gismeteo" title="Gismeteo" src="http://www.gismeteo.ru/static/images/informer2/logo-mini2.png" align="absmiddle" border="0" />
+                       <span>Gismeteo</span>
+                     </a>
+                   </div>
+                   <div class="rightCol">
+                     <a href="http://www.gismeteo.ru/city/weekly/11855/" target="_blank">Прогноз на 2 недели</a>
+                   </div>
+                   </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+        <script src="http://www.gismeteo.ru/ajax/getInformer/?hash=P2K37C0Nb81K4L" type="text/javascript"></script>
+        <!-- Gismeteo informer END -->
+
+        <div><script charset="UTF-8" src="http://widget24.com/code/calendar?data%5BWidget%5D%5Bcss%5D=white" type="text/javascript"></script></div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6" ng-hide="loading">
         <div class="thumbnail" ng-repeat="feed in feeds.responseData.feed.entries | filter:search">
             <div class="caption">
                 <h3 id="tmb-title">
@@ -31,20 +65,22 @@ Page | feed
     </div>
 
     <div class="col-sm-3 col-md-3 col-lg-3 hidden-xs" ng-hide="loading">
-        <select class="form-control" ng-model="search" >
+        <label for="select_source">Выбор источника:</label>
+        <select class="form-control" id="select_source" ng-model="search" >
           <optgroup label="Выбор источника:">
-              <option>lenta.ru</option>
-              <option>lifenews.ru</option>
               <option>rusvesna.su</option>
-              <option>censor.net.ua</option>
               <option>unian.net</option>
+              <option>lenta.ru</option>
+              <option>liga.net</option>
+              <option>lifenews.ru</option>
+              <option>censor.net.ua</option>
           </optgroup>
           <optgroup label="Очистка выбора(пустой фильтр):">
             <option></option>
           </optgroup>
         </select>
 
-
+        &nbsp
         <div class="thumbnail" ng-repeat="image in images.responseData.feed.entries">
             <div class="caption">
                 <img class="tmbimg" src="{{ image.content.match(murl)[1] }}" alt=""/><hr/>
